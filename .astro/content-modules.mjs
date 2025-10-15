f36c253026471b102e3500684bc3886a897a04b0
alex <alexnawotka@pm.me>
2025-10-14 22:35:04 -0400

    [WIP] first bb steps

--- .astro/content-modules.mjs
+++ .astro/content-modules.mjs
@@ -1,8 +1,8 @@
 
 export default new Map([
 ["src/content/writing/venus-enters-taurus.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fwriting%2Fvenus-enters-taurus.mdx&astroContentModuleFlag=true")],
+["src/content/page/About.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fpage%2FAbout.mdx&astroContentModuleFlag=true")],
+["src/content/page/Booking.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fpage%2FBooking.mdx&astroContentModuleFlag=true")],
 ["src/content/page/Home.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fpage%2FHome.mdx&astroContentModuleFlag=true")],
-["src/content/page/Writing.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fpage%2FWriting.mdx&astroContentModuleFlag=true")],
-["src/content/page/Offerings.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fpage%2FOfferings.mdx&astroContentModuleFlag=true")],
 ["src/content/page/settings.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fpage%2Fsettings.mdx&astroContentModuleFlag=true")]]);
 		
