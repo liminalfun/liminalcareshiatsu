399760bc53b042f8ef6c512a24f0d4900c09a32f
alex <alexnawotka@pm.me>
2025-10-27 15:32:08 -0400

    MVP almost ready!

--- .astro/content-modules.mjs
+++ .astro/content-modules.mjs
@@ -1,8 +1,5 @@
 
 export default new Map([
-["src/content/writing/venus-enters-taurus.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fwriting%2Fvenus-enters-taurus.mdx&astroContentModuleFlag=true")],
-["src/content/page/About.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fpage%2FAbout.mdx&astroContentModuleFlag=true")],
-["src/content/page/Booking.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fpage%2FBooking.mdx&astroContentModuleFlag=true")],
 ["src/content/page/Home.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fpage%2FHome.mdx&astroContentModuleFlag=true")],
 ["src/content/page/settings.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fpage%2Fsettings.mdx&astroContentModuleFlag=true")]]);
 		
